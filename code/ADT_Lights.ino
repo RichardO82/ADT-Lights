bbabe01b73f1c32b162cc487c6c0453004c35886
RichardO82 <richard.overman@gmail.com>
2025-02-05 17:58:10 -0800

    Update ADT_Lights.ino

--- code/ADT_Lights.ino
+++ code/ADT_Lights.ino
@@ -791,7 +791,7 @@ void ReCalcADT(void) {
       break;
   }
 
-  yearFraction += 171.0;        // Offset year to start at Jun 21st (spring equinox).
+  yearFraction += 171.0;        // Offset year to sychronize the spring equinox with the maximum of the wave (cos(0)).
   if(yearFraction > 364.0) yearFraction -= 365.0;
 
   yearFraction = yearFraction / 365.0;
